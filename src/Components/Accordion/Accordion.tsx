b927066966084e6e35d01cd983f5856c634d6608
Belom7 <Still07pkm@gmail.com>
2021-11-19 19:56:23 +0300

    1. work-components end homework-components Star

---
+++ src/Components/Accordion/Accordion.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Accordion = () =>{
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    )
+}
